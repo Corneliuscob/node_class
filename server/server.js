d585e37b34dc7b9c5a493be45ccb30521e77a230
ngai to <ngai_to_lo@hotmail.com>
2018-06-07 22:19:55 -0700

    started node with templated engine handlebars

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
 const express = require('express');
-app.listen(3000,()=>{
-    console.log("listening on port 3000");
-});
+const hbs = require('hbs');
+
 
 var app = express();
 
 
+
+app.listen(3000,()=>{
+    console.log("listening on port 3000");
+});
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',(req,res)=>{
     // res.send('<h1>Hello</h1>');
-    res.send({
-        name: 'Toto',
-        likes: ['cheese', 'chess']
-    });
+    // res.send({
+    //     name: 'Toto',
+    //     likes: ['cheese', 'chess']
+    // });
+    res.render('home.hbs', { objectproperty : 'some properties to inject into the template',
+                            pageTitle: 'About Page',
+                            currentYear : new Date().getFullYear(),
+                            welcome_Message: 'Welcome to my rendered webpage'
+                            });
 })
 
 app.get('/about',(req,res)=>{
-    res.send('About page');
+    res.render('about.hbs', { objectproperty : 'some properties to inject into the template',
+                            pageTitle: 'About Page',
+                            currentYear : new Date().getFullYear()
+                            });
 });
 
 app.get('/bad',(req,res)=>{
