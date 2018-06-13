89f70048d4c078a20fcace9d4540562142dd977e
ngai to <ngai_to_lo@hotmail.com>
2018-06-12 21:54:47 -0700

    added express middle ware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 
 var app = express();
 
@@ -9,8 +9,47 @@ var app = express();
 app.listen(3000,()=>{
     console.log("listening on port 3000");
 });
+
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var log = new Date().toString();
+    console.log(`${log}: ${req.method} ${req.url}`);
+    fs.appendFile('server.log',log + '\n',(err)=>{
+        if (err){
+            console.log('Unable to appened to the server.log');
+        }
+    })
+
+    next();
+});
+    // on instances where  we dont want to continue we would never print the next()
+    // an example used is if hte site is under maintenance 
+app.use((req,res,next)=>{
+    var mflag = false;
+    if(mflag){
+        res.render('maintenance.hbs');
+    }
+    else{
+        next();
+    }
+});
+
 app.use(express.static(__dirname + '/public'));
+//express middleware
+
+
+// helpers are functions we can pass into handle bars; this helper is passed into footer.hbs
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+// helpers can also take in arguments. This is very useful; this helper turns any string into upper case
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
 
 app.get('/',(req,res)=>{
     // res.send('<h1>Hello</h1>');
@@ -19,8 +58,7 @@ app.get('/',(req,res)=>{
     //     likes: ['cheese', 'chess']
     // });
     res.render('home.hbs', { objectproperty : 'some properties to inject into the template',
-                            pageTitle: 'About Page',
-                            currentYear : new Date().getFullYear(),
+                            pageTitle: 'Home Page',
                             welcome_Message: 'Welcome to my rendered webpage'
                             });
 })
@@ -28,7 +66,11 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.render('about.hbs', { objectproperty : 'some properties to inject into the template',
                             pageTitle: 'About Page',
-                            currentYear : new Date().getFullYear()
+                            });
+});
+app.get('/help',(req,res)=>{
+    res.render('help.hbs', { objectproperty : 'some properties to inject into the template',
+                            pageTitle: 'Help Page',
                             });
 });
 
